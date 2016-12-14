90e25f69d41740b90af0f04d7e222db325549f00
Waldir Martinez Consuegra <Waldir Martinez Consuegra>
2016-12-14 02:01:18 -0500

    Update layout

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,15 @@
 (function () {
     'use strict';
-    
+
     var app = angular.module('cart', [
         'routes',
         'productlist.controller'
     ]);
-    
-    $('.ui.sticky').sticky({context: '#content', offset: 92});
+
+    $('.ui.sticky').sticky({context: '#content', offset: 70});
     $('.ui.dropdown').dropdown();
-    
+    $('.card .image').dimmer({
+        on: 'hover'
+    });
+
 }());
